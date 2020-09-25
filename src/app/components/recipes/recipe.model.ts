adee520aaaec2fb2a59dfef48abaed539a4e7ebb
Raffael Martins <rbarbosama@indracompany.com>
2020-09-25 14:11:57 -0300

    Creating a Recipe Model

---
+++ src/app/components/recipes/recipe.model.ts
@@ -0,0 +1,14 @@
+import { Module } from "module";
+
+export class Recipe {
+    public name: string;
+    public description: string;
+    public imagePath: string;
+
+    constructor(name: string, description: string, imagePath: string) {
+        this.name = name;
+        this.description = description;
+        this.imagePath = imagePath;
+    }
+
+}
