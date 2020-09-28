df4d59eb5dcd43c1517cbeb2f5f28d174f36aefc
Raffael Martins <rbarbosama@indracompany.com>
2020-09-28 14:25:19 -0300

    Adding a Shopping List Edit Section

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,6 @@
 import { RecipesService } from './../recipes.service';
 import { Component, OnInit } from '@angular/core';
-import { Recipe } from '../recipe.model';
+import { Recipe } from '../../../models/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
