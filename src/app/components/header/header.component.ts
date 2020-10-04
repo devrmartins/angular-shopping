4c6273d9f8700fa0a8b738ebb19f335b45ed37b2
Raffael Martins <rbarbosama@indracompany.com>
2020-10-04 12:00:37 -0300

    Passing Data with Property Binding

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
-    selector: 'app-header',
-    templateUrl: './header.component.html'
+  selector: 'app-header',
+  templateUrl: './header.component.html',
 })
-export class HeaderComponent { 
-    collapsed: boolean = true;
-    manageMenuOpened: boolean = false;
+export class HeaderComponent {
+  collapsed: boolean = true;
+  manageMenuOpened: boolean = false;
+  @Output('onFeatureSelected') featureSelected = new EventEmitter<String>();
+
+  onSelect(feature: String) {
+    this.featureSelected.emit(feature);
+  }
 }
