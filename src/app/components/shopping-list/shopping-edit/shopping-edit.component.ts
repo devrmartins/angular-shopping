3de6f7e5951127fc5b346d43df7b8a8e0d8ef35a
Raffael Martins <rbarbosama@indracompany.com>
2021-01-07 12:23:58 -0300

    Allowing the User to Add Ingredients

--- src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/components/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Ingredient } from './../../../models/ingredient.model';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent implements OnInit {
+  @Output() onAddIngredient = new EventEmitter<Ingredient>();
+  ingredientForm = new FormGroup({
+    name: new FormControl(''),
+    amount: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  addIngredient() {
+    const ingredient = new Ingredient(
+      this.ingredientForm.get('name').value,
+      this.ingredientForm.get('amount').value
+    );
+    this.onAddIngredient.emit(ingredient);
   }
-
 }
